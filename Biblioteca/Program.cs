bf20d3825db9d6d812fcaa121fb2328a010a8d07
cmsagardia19-a11y <cmsagardia19@icloud.com>
2026-09-16 20:50:15 -0300

    Agregar archivos de proyecto.

---
+++ Biblioteca/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Colecciones
+{
+    internal class test
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca biblioteca = new Biblioteca();
+            cargarLibros(10);
+            cargarLibros(2);
+            biblioteca.ListarLibros();
+            biblioteca.EliminarLibro("Libro5");
+            biblioteca.ListarLibros();
+            void cargarLibros(int cantidad)
+            {
+                bool pude;
+                for (int i = 1; i <= cantidad; i++)
+                {
+                    pude = biblioteca.AgregarLibro("Libro" + i, "Autor" + i, "Editorial" + i);
+                    if (pude)
+                        Console.WriteLine("libro" + i + "Agregado correctamente");
+                    else
+                        Console.WriteLine("libro" + i + "Ya existe en biblioteca");
+                }
+
+            }
+        }
+    }
+}
